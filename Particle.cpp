7f9417a35bdad24b692c3375a48a96add873c451
dluque <dluque@S5-20>
2017-03-08 13:23:09 +0100

    inicialización de verlet arreglada

--- Particle.cpp
+++ Particle.cpp
@@ -3,18 +3,18 @@
 
 Particle::Particle() {
 	//setup the initial parameters (position)
-	currentPos.x = 0;
-	currentPos.y = 5;
-	currentPos.z = 0;
-
-	lastPos.x = (((float)rand() / RAND_MAX) * 10.f - 5.f) * (1 / 30);
-	lastPos.y = (((float)rand() / RAND_MAX) * 10.f - 5.f) * (1 / 30);
-	lastPos.z = (((float)rand() / RAND_MAX) *  10.f - 5.f) * (1 / 30);
+	currentPos.x = 0.f;
+	currentPos.y = 5.f;
+	currentPos.z = 0.f;
 
 	//setup the initial speed
 	currentV.x = ((float)rand() / RAND_MAX) * 10.f - 5.f;
 	currentV.y = ((float)rand() / RAND_MAX) * 10.f - 5.f;
 	currentV.z = ((float)rand() / RAND_MAX) * 10.f - 5.f;
+
+	lastPos.x = currentPos.x - currentV.x * (1.f / 30.f);
+	lastPos.y = currentPos.y - currentV.y * (1.f / 30.f);
+	lastPos.z = currentPos.z - currentV.z * (1.f / 30.f);
 
 	lastV.x = currentV.x;
 	lastV.y = currentV.y;
